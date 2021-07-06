adb8ed42a7ecfad1a3d69adeba32a4d5072ffe7c
KeerthanaBoppasamudram <74814976+KeerthanaBoppasamudram@users.noreply.github.com>
2021-07-06 16:18:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var allPlayers
+var database;
+var gameState=0;
+var playerCount=0;
+var form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(400,400);
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+  background("white");
+  
+}
